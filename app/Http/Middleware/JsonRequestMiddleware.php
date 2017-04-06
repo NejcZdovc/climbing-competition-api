419e5f696bbdeda3b3edf62e0c2d657081dbb42a
NejcZdovc <nejc.zdovc@3zsistemi.si>
2017-04-06 13:29:35 +0200

    Fixed CORS problem

--- app/Http/Middleware/JsonRequestMiddleware.php
+++ app/Http/Middleware/JsonRequestMiddleware.php
@@ -5,12 +5,36 @@ use Illuminate\Http\Request;
 use Log;
 
 class JsonRequestMiddleWare {
-    public function handle(Request $request, Closure $next) {
-        if (in_array($request->method(), ['POST', 'PUT', 'PATCH']) && $request->isJson()) {
-            $data = $request->json()->all();
-            $request->request->replace(is_array($data) ? $data : []);
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        header_remove('Access-Control-Allow-Origin');
+
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
+        if ($request->isMethod('OPTIONS'))
+        {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
 
-        return $next($request);
+        $response = $next($request);
+        foreach($headers as $key => $value)
+        {
+            $response->header($key, $value);
+        }
+
+        return $response;
     }
 }
